aa9f967fc5897250a40bf7f6c51343457f922a6c
Clint.Network <me@clint.network>
2020-10-16 22:24:30 +0200

    Publish

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Linq;
-using System.Threading;
-using ClintSharp;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Enums;
 using OpenQA.Selenium.Appium.Service;
-using OpenQA.Selenium.Support.UI;
 
 namespace IGAccountCreatorBeta
 {
@@ -31,9 +28,9 @@ namespace IGAccountCreatorBeta
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "10.0");
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.NoReset, true);
-            appiumOptions.AddAdditionalCapability(MobileCapabilityType.NewCommandTimeout, "120");
-            appiumOptions.AddAdditionalCapability("preventWDAAttachments", false);
-            appiumOptions.AddAdditionalCapability("clearSystemFiles", true);
+            // appiumOptions.AddAdditionalCapability(MobileCapabilityType.NewCommandTimeout, "120");
+            // appiumOptions.AddAdditionalCapability("preventWDAAttachments", false);
+            // appiumOptions.AddAdditionalCapability("clearSystemFiles", true);
             appiumOptions.AddAdditionalCapability("appPackage", "com.instagram.android");
             appiumOptions.AddAdditionalCapability("appActivity", "com.instagram.android.activity.MainTabActivity");
             // appiumOptions.AddAdditionalCapability("forceMjsonwp", true);
@@ -42,61 +39,37 @@ namespace IGAccountCreatorBeta
             _driver = new AndroidDriver<AppiumWebElement>(_appiumLocalService, appiumOptions);
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
             
-            Console.WriteLine(_driver.PageSource);
-            
-            var multiLogin = _driver.FindElements(By.Id("com.google.android.gms:id/credential_picker_layout"));
-            Console.WriteLine($"Is multi login? {multiLogin}");
-            if (multiLogin.Any())
-            {
-                Console.WriteLine("vire le multi login");
-                _driver.FindElement(By.Id("com.google.android.gms:id/cancel")).Click();
-                // Thread.Sleep(TimeSpan.FromSeconds(15));
-            }
-
-            var isLoginFormPresent = _driver.FindElements(By.Id("com.instagram.android:id/log_in_button"));
-            Console.WriteLine($"Is login form is present? {isLoginFormPresent.Any()}");
-            if (isLoginFormPresent.Any() && isLoginFormPresent.First().Text.ToLower().Contains("sign up"))
-            {
-                Console.WriteLine("signup first");
-                isLoginFormPresent.First().Click();
-                // Thread.Sleep(TimeSpan.FromSeconds(15));
-            }
-            
             var signupCta = _driver.FindElements(By.Id("com.instagram.android:id/sign_up_with_email_or_phone"));
             Console.WriteLine($"Is setup CTA is present? {signupCta.Any()}");
             if (signupCta.Any())
             {
-                Console.WriteLine("signup second");
-                _driver.FindElement(By.Id("com.instagram.android:id/sign_up_with_email_or_phone")).Click();
-                // Thread.Sleep(TimeSpan.FromSeconds(15));
-                Console.WriteLine("right tab");
+                signupCta.First().Click();
                 _driver.FindElement(By.Id("com.instagram.android:id/right_tab")).Click();
-                // Thread.Sleep(TimeSpan.FromSeconds(15));
-                // _driver.HideKeyboard();
-                // Thread.Sleep(TimeSpan.FromSeconds(3));
-                // var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(27));
-                // wait.Until(x => x.FindElement(By.Id("com.instagram.android:id/email_field"))).SendKeys("shanelle.dewey9826@outlook.com");
-                Console.WriteLine("email field");
-                _driver.FindElement(By.Id("com.instagram.android:id/email_field")).SendKeys("shanelle.dewey9826@outlook.com");
-                // Thread.Sleep(TimeSpan.FromSeconds(3));
-                Console.WriteLine("next button");
+                
+                Console.Write("Enter email: ");
+                var email = Console.ReadLine();
+                _driver.FindElement(By.Id("com.instagram.android:id/email_field")).SendKeys(email);
                 _driver.FindElement(By.Id("com.instagram.android:id/right_tab_next_button")).Click();
-                // Thread.Sleep(TimeSpan.FromSeconds(15));
                 
-                var isAlreadyUsedEmail = _driver.FindElements(By.Id("com.instagram.android:id/negative_button"));
+                // is already existing account
+
+                Console.Write("Confirmation code: ");
+                var confirmationCode = Console.ReadLine();
                 
-                Console.WriteLine(isAlreadyUsedEmail
-                    .Select(x => x.Text)
-                    .ToJsonF());
+                _driver.FindElement(By.Id("com.instagram.android:id/confirmation_code")).SendKeys(confirmationCode);
+                _driver.FindElement(By.Id("com.instagram.android:id/next_button")).Click();
+            
+                _driver.FindElement(By.Id("com.instagram.android:id/full_name")).SendKeys("Nom complet");
+                _driver.FindElement(By.Id("com.instagram.android:id/password")).SendKeys("jdks3202390");
+                _driver.FindElement(By.Id("com.instagram.android:id/remember_password_checkbox")).Click();
+                _driver.FindElement(By.Id("com.instagram.android:id/continue_without_ci")).Click();
 
-                Console.WriteLine("END OF LIFE");
-                Console.ReadLine();
-
-                if (isAlreadyUsedEmail.Any())
-                {
-                    isAlreadyUsedEmail.First().Click();
-                }
-
+                _driver.FindElement(By.Id("com.instagram.android:id/next_button")).Click();
+                _driver.FindElement(By.Id("com.instagram.android:id/primary_button")).Click();
+                _driver.FindElement(By.Id("com.instagram.android:id/add_age_link")).Click();
+            
+                _driver.FindElement(By.Id("com.instagram.android:id/entered_age")).SendKeys("21");
+                _driver.FindElement(By.Id("com.instagram.android:id/next_button")).Click();
             }
 
         }
