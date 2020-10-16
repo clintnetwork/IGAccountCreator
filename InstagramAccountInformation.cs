301402d9f99294e956b48c2547219f6538ea29ea
Clint.Network <me@clint.network>
2020-10-17 00:29:30 +0200

    Working MVP

---
+++ InstagramAccountInformation.cs
@@ -0,0 +1,13 @@
+namespace IGAccountCreator
+{
+    public class InstagramAccountInformation
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        public string ProfilePicture { get; set; }
+        public string Bio { get; set; }
+        public string Age { get; set; }
+    }
+}
--- InstagramAutomator.cs
+++ InstagramAutomator.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using ClintSharp;
+using CsvHelper;
 using MailKit;
 using MailKit.Net.Imap;
 using OpenQA.Selenium;
@@ -14,9 +19,9 @@ namespace IGAccountCreator
 {
     public class InstagramAutomator
     {
-        private static AppiumLocalService _appiumLocalService;
         private static AndroidDriver<AppiumWebElement> _driver;
-        
+        private List<InstagramAccountInformation> _records;
+
         public InstagramAutomator()
         {
             Environment.SetEnvironmentVariable(AppiumServiceConstants.NodeBinaryPath, "/usr/local/bin/node");
@@ -24,39 +29,44 @@ namespace IGAccountCreator
             Environment.SetEnvironmentVariable("ANDROID_HOME", "/Users/clint.network/Library/Android/sdk");
             Environment.SetEnvironmentVariable("JAVA_HOME", "/Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home");
 
-            _appiumLocalService = new AppiumServiceBuilder().UsingAnyFreePort().Build();
-            _appiumLocalService.Start();
+            var appiumLocalService = new AppiumServiceBuilder().UsingAnyFreePort().Build();
+            appiumLocalService.Start();
 
             var appiumOptions = new AppiumOptions();
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "EML-L29");
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "10.0");
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.NoReset, false);
-            // appiumOptions.AddAdditionalCapability(MobileCapabilityType.NewCommandTimeout, "120");
-            // appiumOptions.AddAdditionalCapability("preventWDAAttachments", false);
-            // appiumOptions.AddAdditionalCapability("clearSystemFiles", true);
             appiumOptions.AddAdditionalCapability("appPackage", "com.instagram.android");
             appiumOptions.AddAdditionalCapability("appActivity", "com.instagram.android.activity.MainTabActivity");
-            // appiumOptions.AddAdditionalCapability("forceMjsonwp", true);
             
             Console.WriteLine("Starting...");
-            _driver = new AndroidDriver<AppiumWebElement>(_appiumLocalService, appiumOptions);
+            _driver = new AndroidDriver<AppiumWebElement>(appiumLocalService, appiumOptions);
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
         }
 
         public void LoadCsv(string inputFile)
         {
+            using var reader = new StreamReader(inputFile);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csv.Configuration.Delimiter = ";";
+            _records = csv.GetRecords<InstagramAccountInformation>().ToList();
         }
         
         public void Run()
         {
-            Console.Write("Enter email: ");
-            var email = Console.ReadLine();
-            Console.Write("Enter password: ");
-            var password = Console.ReadLine();
-            Console.Write("Enter username: ");
-            var username = Console.ReadLine();
-            
+            foreach (var record in _records)
+            {
+                Console.WriteLine($"Creating account:\n{record.ToJsonF()}");
+                RegisterAccount(record);
+                ChangeProfilePicture(record.ProfilePicture);
+                ChangeFullName(record.Fullname);
+                ChangeBiography(record.Bio);
+            }
+        }
+
+        private void RegisterAccount(InstagramAccountInformation record)
+        {
             var signupCta = _driver.FindElements(By.Id("com.instagram.android:id/sign_up_with_email_or_phone"));
             Console.WriteLine($"Is setup CTA is present? {signupCta.Any()}");
             if (signupCta.Any())
@@ -64,12 +74,12 @@ namespace IGAccountCreator
                 signupCta.First().Click();
                 _driver.FindElement(By.Id("com.instagram.android:id/right_tab")).Click();
                 
-                _driver.FindElement(By.Id("com.instagram.android:id/email_field")).SendKeys(email);
+                _driver.FindElement(By.Id("com.instagram.android:id/email_field")).SendKeys(record.Email);
                 _driver.FindElement(By.Id("com.instagram.android:id/right_tab_next_button")).Click();
                 
                 // is already existing account
                 
-                var confirmationCode = GetConfirmationCode(email, password);
+                var confirmationCode = GetConfirmationCode(record.Email, record.Password);
                 if (confirmationCode.IsNull())
                 {
                     throw new Exception("Unable to get confirmation code");
@@ -78,8 +88,8 @@ namespace IGAccountCreator
                 _driver.FindElement(By.Id("com.instagram.android:id/confirmation_code")).SendKeys(confirmationCode);
                 _driver.FindElement(By.Id("com.instagram.android:id/next_button")).Click();
             
-                _driver.FindElement(By.Id("com.instagram.android:id/full_name")).SendKeys("Nom complet");
-                _driver.FindElement(By.Id("com.instagram.android:id/password")).SendKeys("jdks3202390");
+                _driver.FindElement(By.Id("com.instagram.android:id/full_name")).SendKeys(record.Fullname);
+                _driver.FindElement(By.Id("com.instagram.android:id/password")).SendKeys(record.Password);
                 _driver.FindElement(By.Id("com.instagram.android:id/remember_password_checkbox")).Click();
                 _driver.FindElement(By.Id("com.instagram.android:id/continue_without_ci")).Click();
 
@@ -87,11 +97,11 @@ namespace IGAccountCreator
                 _driver.FindElement(By.Id("com.instagram.android:id/primary_button")).Click();
                 _driver.FindElement(By.Id("com.instagram.android:id/add_age_link")).Click();
             
-                _driver.FindElement(By.Id("com.instagram.android:id/entered_age")).SendKeys("21");
+                _driver.FindElement(By.Id("com.instagram.android:id/entered_age")).SendKeys(record.Age);
                 _driver.FindElement(By.Id("com.instagram.android:id/next_button")).Click();
                 
                 _driver.FindElement(By.Id("com.instagram.android:id/change_username")).Click();
-                _driver.FindElement(By.Id("com.instagram.android:id/username")).SendKeys(username);
+                _driver.FindElement(By.Id("com.instagram.android:id/username")).SendKeys(record.Username);
                 _driver.FindElement(By.Id("com.instagram.android:id/next_button")).Click();
                 
                 _driver.FindElement(By.Id("com.instagram.android:id/skip_button")).Click();
@@ -100,6 +110,76 @@ namespace IGAccountCreator
                 _driver.FindElement(By.Id("com.instagram.android:id/skip_button")).Click();
                 _driver.FindElement(By.Id("com.instagram.android:id/action_bar_button_action")).Click();
             }
+        }
+
+        private static void ChangeProfilePicture(string profilePicture)
+        {
+            try
+            {
+                var extension = new FileInfo(profilePicture).Extension.Replace(".", "");
+                var photoBytes = new WebClient().DownloadData(profilePicture);
+            
+                // TODO: Check if the path is always the same
+                _driver.PushFile($"/storage/emulated/0/DCIM/Camera/{Guid.NewGuid()}.{(string.IsNullOrWhiteSpace(extension) ? "jpg" : extension)}", photoBytes);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Unable to download the picture ({e.Message})");
+            }
+            
+            _driver
+                .FindElement(By.Id("com.instagram.android:id/profile_tab"))
+                .Click();
+
+            _driver
+                .FindElement(By.Id("com.instagram.android:id/coordinator_root_layout"))
+                .FindElements(By.ClassName("android.widget.Button"))
+                .First(x => x.Text.ToLower().Contains("edit profile"))
+                .Click();
+            
+            _driver.FindElement(By.Id("com.instagram.android:id/change_avatar_button")).Click();
+            _driver.FindElement(By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.FrameLayout[1]")).Click();
+            _driver.FindElement(By.Id("com.instagram.android:id/next_button_textview")).Click();
+            _driver.FindElement(By.Id("com.instagram.android:id/next_button_textview")).Click();
+        }private static void ChangeFullName(string fullname)
+        {
+            _driver
+                .FindElement(By.Id("com.instagram.android:id/profile_tab"))
+                .Click();
+
+            _driver
+                .FindElement(By.Id("com.instagram.android:id/coordinator_root_layout"))
+                .FindElements(By.ClassName("android.widget.Button"))
+                .First(x => x.Text.ToLower().Contains("edit profile"))
+                .Click();
+            
+            _driver.FindElement(By.Id("com.instagram.android:id/full_name")).Click();
+            _driver.FindElement(By.Id("com.instagram.android:id/full_name")).FindElement(By.ClassName("android.widget.EditText")).Clear();
+            _driver.FindElement(By.Id("com.instagram.android:id/full_name")).FindElement(By.ClassName("android.widget.EditText")).SendKeys(fullname);
+            // _appium.Driver.FindElement(By.Id("com.instagram.android:id/caption_edit_text")).Clear();
+            // _appium.Driver.FindElement(By.Id("com.instagram.android:id/caption_edit_text")).SendKeys(fullname);
+            _driver.FindElement(By.XPath(@"//android.widget.ViewSwitcher[@content-desc=""Done""]/android.widget.ImageView")).Click();
+            
+            _driver.FindElement(By.XPath(@"//android.widget.ViewSwitcher[@content-desc=""Done""]/android.widget.ImageView")).Click();
+        }
+
+        private static void ChangeBiography(string bio)
+        {
+            _driver
+                .FindElement(By.Id("com.instagram.android:id/profile_tab"))
+                .Click();
+
+            _driver
+                .FindElement(By.Id("com.instagram.android:id/coordinator_root_layout"))
+                .FindElements(By.ClassName("android.widget.Button"))
+                .First(x => x.Text.ToLower().Contains("edit profile"))
+                .Click();
+            
+            _driver.FindElement(By.Id("com.instagram.android:id/bio")).Click();
+            _driver.FindElement(By.Id("com.instagram.android:id/caption_edit_text")).Clear();
+            _driver.FindElement(By.Id("com.instagram.android:id/caption_edit_text")).SendKeys(bio);
+            _driver.FindElement(By.XPath(@"//android.widget.ViewSwitcher[@content-desc=""Done""]/android.widget.ImageView")).Click();
+            _driver.FindElement(By.XPath(@"//android.widget.ViewSwitcher[@content-desc=""Done""]/android.widget.ImageView")).Click();
         }
 
         private static string GetConfirmationCode(string email, string password)
